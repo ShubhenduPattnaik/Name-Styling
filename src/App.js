4ac51a72e7311c9706eec60873e6e10bcbeba0c7
Shubhendu Pattnaik <pattnaikshubhendu@gmail.com>
2023-10-07 16:44:28 +0530

    Initial Phase of the Development. Functionality Complete!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { Fragment, useState } from 'react';
+import Font from './Font/Font';
 
 function App() {
+
+  let [valueEntered, setValue] = useState('');
+  let [fontValue, setFontValue] = useState('');
+
+  const valueChangeHandler = (event) => {
+    setValue(event.target.value);
+  };
+
+  const fontHandler = (event) => {
+    setFontValue(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div id="name">
+        <p style={{ fontFamily: fontValue }} placeholder='Enter a Text Here'>{valueEntered}</p>
+      </div>
+      <div id="value-input">
+        <input type="text" value={valueEntered} onChange={valueChangeHandler} placeholder='Enter a Text Here' />
+        <select name="font" value={fontValue} onChange={fontHandler}>
+          {
+            Font.map(font => <option value={font}>{font}</option>)
+          }
+        </select>
+      </div>
+    </Fragment>
   );
 }
 
